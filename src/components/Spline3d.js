8259160a53d478d1fe7f3e63b2766abcadd6f879
Beyazıt Bestami Sarıkaya <61268112+BBestamiS@users.noreply.github.com>
2022-04-22 21:18:10 +0300

    fixed 3d rescale problem

--- src/components/Spline3d.js
+++ src/components/Spline3d.js
@@ -27,7 +27,7 @@ function Spline3d() {
     const camera = spline.findObjectById('00e46ed7-0378-45fd-946d-aca0a8a93189')
     const obj = spline.findObjectById('d4d43fbc-8ed0-4b14-a6ca-acfbd8848c83')
     if (obj != null && camera != null) {
-      // console.log(window.innerHeight)
+      console.log(spline);
       const camareAspectRatio = (800 / window.innerWidth);
       if (camareAspectRatio > 0.8) {
         camera.scale.x = camareAspectRatio;
@@ -36,35 +36,19 @@ function Spline3d() {
       setMyCameraPos(camera)
       setMyObjPos(obj.position);
     }
-    // spline.canvas.style.width = "100%"
-    // spline.canvas.style.height = "100%"
-    // setMyObj(spline)
   }
-  if (myCameraPos) {
-
-    // console.log(myCameraPos)
-  }
-
   if (width) {
-    console.log(width)
+    // console.log(width)
     if (myCameraPos) {
-      // console.log(width)
       myCameraPos.scale.x = (800 / width)
       myCameraPos.scale.y = (800 / width)
-      console.log(myCameraPos.scale.x)
     }
   }
-
-
-  // useEffect(() => {
-  //   setMyObj(document.getElementById('spline'));
-  // })
 
   return (
     <div id='spline3d' >
       <div id='git-div'>
-        {/* <Spline scene={Git} onLoad={onLoad} /> */}
-        <Spline autoRender={false} style={{ width: "100vw", aspectRatio: "1852/832" }} scene={Git} onLoad={onLoad} id="spline" />
+        <Spline scene={Git} onLoad={onLoad} id="spline" />
       </div>
     </div>
   );
