08f21a6571ec7b36b48ea2eab76d24008e299274
Beyazıt Bestami Sarıkaya <61268112+BBestamiS@users.noreply.github.com>
2022-05-07 00:14:07 +0300

    fixed some animation problem

--- src/components/Spline3d.js
+++ src/components/Spline3d.js
@@ -1,8 +1,9 @@
 import Spline from '@splinetool/react-spline';
 import Git from '../3d/git.spline'
 import '../styles/spline3d-style.scss'
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useState } from 'react'
+import Animation from './Animation';
 import { Link } from 'react-router-dom';
 
 function useWindowSize() {
@@ -24,38 +25,42 @@ function Spline3d() {
   const [myObjPos, setMyObjPos] = useState(null);
   const [myCameraPos, setMyCameraPos] = useState(null);
   const [width, height] = useWindowSize();
+  const camareAspectRatio = (1200 / window.innerWidth);
   function onLoad(spline) {
-    const camera = spline.findObjectById('00e46ed7-0378-45fd-946d-aca0a8a93189')
+
+    const camera = spline.findObjectById('112c0ce8-2470-4e2f-b92a-2a9ad2b1405c')
     const obj = spline.findObjectById('d4d43fbc-8ed0-4b14-a6ca-acfbd8848c83')
     if (obj != null && camera != null) {
       // console.log(spline);
-      const camareAspectRatio = (800 / window.innerWidth);
-
       if (camareAspectRatio > 0.8) {
         camera.scale.x = camareAspectRatio;
         camera.scale.y = camareAspectRatio;
-      }
+      } else {
+        camera.scale.x = 0.8;
+        camera.scale.y = 0.8;
 
+      }
       setMyCameraPos(camera)
       setMyObjPos(obj.position);
     }
   }
   if (width) {
     if (myCameraPos) {
-
-      myCameraPos.scale.y = (1000 / width)
-      myCameraPos.scale.x = (1000 / width)
+      if (camareAspectRatio > 0.8) {
+        myCameraPos.scale.y = camareAspectRatio
+        myCameraPos.scale.x = camareAspectRatio
+      }
     }
   }
 
   return (
+
     <div id='spline3d' >
-      <div id='git-div'>
-        <a target="_blank" href="https://github.com/BBestamiS" id="unsplash-last-photo">
-          <Spline scene={Git} onLoad={onLoad} id="spline" />
-        </a>
+      <div id='git-div' >
+        <Spline scene={Git} onLoad={onLoad} id="spline" />
       </div>
     </div>
   );
+
 }
 export default Spline3d;
